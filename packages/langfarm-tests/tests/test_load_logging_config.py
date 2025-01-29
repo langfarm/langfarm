dd8c85497a2a2d7fc9eb421f8a2c28f1d7d5335b
chenlb <chenlbphoto@163.com>
2025-01-29 10:28:02 +0800

    feat: 增加模块 langfarm-tests 用于测试的基础。使用 testcontainers、logger 配置。 (#3)

---
+++ packages/langfarm-tests/tests/test_load_logging_config.py
@@ -0,0 +1,8 @@
+from langfarm_tests.base_for_test import get_test_logger
+
+logger = get_test_logger(__name__)
+
+
+def test_load_test_config():
+    logger.info("load_test_config success")
+    assert True
