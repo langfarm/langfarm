ae32c7632771afc3589daff738ac6b6449e6b83e
chenlb <chenlbphoto@163.com>
2025-02-12 23:31:47 +0800

    feat: 向 kafka 发送信息的基础功能。

--- apps/langfarm-tracing/src/langfarm_tracing/config.py
+++ apps/langfarm-tracing/src/langfarm_tracing/config.py
@@ -1,10 +1,26 @@
+from pydantic import BaseModel
+from pydantic import (
+    computed_field,
+)
+from confluent_kafka import Producer
 from langfarm_app_base.config import AppSettings, PostgresConfig, LangfuseConfig
 from langfarm_io.redis import RedisConfig
+from langfarm_tracing import kafka
 
 
-class Settings(AppSettings, LangfuseConfig, PostgresConfig, RedisConfig):
+class KafkaConfig(BaseModel):
+    KAFKA_BOOTSTRAP_SERVERS: str
+
+
+class Settings(AppSettings, LangfuseConfig, PostgresConfig, RedisConfig, KafkaConfig):  # type: ignore
     API_V1_STR: str = "/api/public"
     PROJECT_NAME: str = "langfarm-tracing"
+
+    # 创建 producer
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def create_kafka_producer(self) -> Producer:
+        return kafka.get_kafka_producer(self)
 
 
 settings: Settings
